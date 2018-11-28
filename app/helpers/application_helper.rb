cf8444f0d0d4365a236f7c45fa573c245628799b
Quentin Girard <quentin.girard@wavemind.ch>
2018-11-28 16:38:33 +0100

    Setup template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+    when 'notice' then
+      'alert alert-success'
+    when 'success' then
+      'alert alert-success'
+    when 'error' then
+      'alert alert-danger'
+    when 'alert' then
+      'alert alert-danger'
+    end
+  end
+
+  def date_format(date)
+    if date.blank?
+      '<em>Non défini</em>'.html_safe
+    else
+      date.strftime('%d.%m.%Y')
+    end
+  end
+
+  def datetime_format(date)
+    if date.blank?
+      '<em>Non défini</em>'.html_safe
+    else
+      date.strftime('%d.%m.%Y à %H:%M')
+    end
+  end
+
+  def time_format(date)
+    if date.blank?
+      '<em>Non défini</em>'.html_safe
+    else
+      date.strftime('%Hh')
+    end
+  end
+
 end
