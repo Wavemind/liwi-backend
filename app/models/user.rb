59565b9a89a709ce3d4e8e3876a6798d28f19d8f
Quentin Girard <quentin.girard@wavemind.ch>
2018-11-29 16:54:24 +0100

    Add model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :lockable
+  belongs_to :role
 
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
+  validates_presence_of :role
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, :lockable
 end
