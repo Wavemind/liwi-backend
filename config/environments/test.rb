862fa613ffe66cf80d821abcc69a4db9fc5b1ebe
Quentin Girard <quentin.girard@wavemind.ch>
2018-12-10 17:01:39 +0100

    Change host url in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,6 +48,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'https://liwi.wavelab.top'}
   config.action_mailer.raise_delivery_errors = true
 end
