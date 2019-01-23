f6374bfb68a21892231e86fdc73159257341987d
Quentin Girard <quentin.girard@wavemind.ch>
2019-01-23 10:31:47 +0100

    Fix diagnostic relation + test unit for it

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -1,10 +1,12 @@
 # How a disease is diagnosed
 class Diagnostic < ApplicationRecord
 
-  has_many :questions
-  has_many :algorithm_version_diagnostics, through: :questions
+  has_many :algorithm_version_diagnostics
+  has_many :algorithm_versions, through: :algorithm_version_diagnostics
 
-  validates_presence_of :value
-  validates_presence_of :display
+  validates_presence_of :reference
+  validates_presence_of :label
+
+  validates_uniqueness_of :reference
 
 end
