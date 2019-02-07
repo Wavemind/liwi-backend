003858c82f0a9fd11ca3b2500fe2ef3d6e418046
Emmanuel <emmanuel.barchichat@wavemind.ch>
2019-02-07 08:43:47 +0000

    remove old route

--- app/controllers/api/v1/algorithm_versions_controller.rb
+++ app/controllers/api/v1/algorithm_versions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::AlgorithmVersionsController < Api::V1::ApplicationController
 
   def index
-    # render json: AlgorithmVersion.includes(:algorithm).to_json(include: { algorithm: { only: [:name, :description]}})
     render json: AlgorithmVersionsService.generate_hash(AlgorithmVersion.first.id)
   end
 
