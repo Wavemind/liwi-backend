e4e1766c0ba94da5df467f7dafcad703a70d04ba
Quentin Girard <quentin.girard@wavemind.ch>
2019-02-07 13:51:41 +0100

    Device can now recieved through the API an algoirthm

--- app/controllers/api/v1/algorithm_versions_controller.rb
+++ app/controllers/api/v1/algorithm_versions_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::AlgorithmVersionsController < Api::V1::ApplicationController
 
   def index
-    device = Device.find_by(mac_address: device_params[:mac_address])
-    algorithm_version = device.group.algorithm_versions.where('group_accesses.end_date IS NULL')
+    # Get the devise make the request
+    device = Device.find_by(mac_address: params[:mac_address])
 
-    if algorithm_version.present?
-      render json: AlgorithmVersion.includes(:algorithm).to_json(include: { algorithm: { only: [:name, :description]}})
+    if device.group.present?
+      # Find the algorithm version available for this group
+      algorithm_version = device.group.algorithm_versions.where('group_accesses.end_date IS NULL').first
+
+      if algorithm_version.present?
+        render json: AlgorithmVersionsService.generate_hash(algorithm_version.id)
+      else
+        render json: {errors: t('.no_algorithm_version')}, status: :unprocessable_entity
+      end
     else
       render json: {errors: t('.no_group')}, status: :unprocessable_entity
     end
