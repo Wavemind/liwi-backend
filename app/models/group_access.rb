5b0c5c8ba89dae2d445a9fcfd73485baa31ab138
Quentin Girard <quentin.girard@wavemind.ch>
2019-02-07 14:11:50 +0100

    Fix attribution of an algorithm version to a group

--- app/models/group_access.rb
+++ app/models/group_access.rb
@@ -1,17 +1,20 @@
 # Intermediary table between groups and algorithm_version
 class GroupAccess < ApplicationRecord
-  after_create :archive_algorithm_version
+  before_create :archive_algorithm_version
 
   belongs_to :group
   belongs_to :algorithm_version
 
   private
 
-  # Callback after_create for archive algoritm_version
+  # Callback before_create for archived an algorithm_version
   def archive_algorithm_version
     last_group_access = GroupAccess.find_by(group_id: self.group_id, end_date: nil)
-    last_group_access.end_date = Time.zone.now
-    last_group_access.access = false
-    last_group_access.save
+
+    if last_group_access.present?
+      last_group_access.end_date = Time.zone.now
+      last_group_access.access = false
+      last_group_access.save
+    end
   end
 end
