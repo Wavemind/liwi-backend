c9c7971cca44743c730e9d6f4bfbdbf81849f6f0
Quentin Girard <quentin.girard@wavemind.ch>
2019-02-15 11:18:47 +0100

    If device doesn't exist, create it through the API

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -2,27 +2,21 @@ class Api::V1::ActivitiesController < ApplicationController
 
   def create
     device_params = activity_params[:device_attributes]
-
     device = Device.find_by_mac_address(device_params[:mac_address])
 
     if device.present?
-      device.mac_address = device_params[:mac_address]
-      device.name = device_params[:name]
-      device.model = device_params[:model]
-      device.brand = device_params[:brand]
-      device.os = device_params[:os]
-      device.os_version = device_params[:os_version]
-
-      activity = Activity.new(activity_params)
-      activity.device = device
-
-      if device.save && activity.save
-        render json: :success
-      else
-        render json: {errors: t('.errors_on_save')}, status: :unprocessable_entity
-      end
+      device.attributes = device_params
     else
-      render json: {errors: t('.errors_device_not_present')}, status: :unprocessable_entity
+      device = Device.create(device_params)
+    end
+
+    activity = Activity.new(activity_params)
+    activity.device = device
+
+    if device.save && activity.save
+      render json: :success
+    else
+      render json: {errors: t('.errors_on_save')}, status: :unprocessable_entity
     end
   end
 
