e35ac7f27e1decba84939926c998d243d7a2d41f
Quentin Girard <quentin.girard@wavemind.ch>
2019-04-30 16:41:15 +0200

    Add css in and out for excluded diagnostic

--- app/javascript/react-diagram/widgets/AdvancedNodeWidget.js
+++ app/javascript/react-diagram/widgets/AdvancedNodeWidget.js
@@ -7,7 +7,8 @@ export interface AdvancedNodeWidgetProps {
   size?: number;
 }
 
-export interface AdvancedNodeWidgetState {}
+export interface AdvancedNodeWidgetState {
+}
 
 /**
  * @author Jean Neige
@@ -25,9 +26,9 @@ class AdvancedNodeWidget extends BaseWidget<AdvancedNodeWidgetProps, AdvancedNod
 
   generateOutPort(port) {
     return (
-      <div key={port.getID()} className="col px-0" style={{position: 'relative'}}>
-          <div className="py-1 text-center answer-split">{port.label}</div>
-          <div className="port out-port" data-name={port.name} data-nodeid={port.parent.id} />
+      <div key={port.getID()} className="col px-0" style={{ position: "relative" }}>
+        <div className="py-1 text-center answer-split">{port.label}</div>
+        <div className="port out-port" data-name={port.name} data-nodeid={port.parent.id}/>
       </div>
     );
   }
@@ -43,35 +44,44 @@ class AdvancedNodeWidget extends BaseWidget<AdvancedNodeWidgetProps, AdvancedNod
 
     let inPort = diagramNode.getInPort();
 
+    console.log(diagramNode);
+
     return (
-      <div className={`node ${(diagramNode.node === 'AND') ? 'and' : ''}`}>
+      <div className={`node ${diagramNode.node === "AND" ? "and" : ""}`}>
         <div className="port py-2 node-category">
-          <div className="port srd-port in-port" data-name={inPort.name} data-nodeid={inPort.parent.id} />
+          <div className="port srd-port in-port" data-name={inPort.name} data-nodeid={inPort.parent.id}/>
           <div className="col pl-2 pr-0 text-left">
-            {(diagramNode.node === 'AND') ? 'AND' : diagramNode.node.reference}
+            {diagramNode.node === "AND" ? "AND" : diagramNode.node.reference}
           </div>
           <div className="col pl-0 pr-2 text-right">
-            {(diagramNode.node === 'AND') ? '' : diagramNode.node.priority}
+            {diagramNode.node === "AND" ? "" : diagramNode.node.priority}
           </div>
         </div>
-        {(diagramNode.node === 'AND') ? (
+        {diagramNode.node === "AND" ? (
           <div>
             <div className="node-answers">
-              <div className="port srd-port" style={{top: 28, left: 8}} data-name={diagramNode.getOutPort().name} data-nodeid={diagramNode.getOutPort().parent.id} />
+              <div className="port srd-port" style={{ top: 28, left: 8 }} data-name={diagramNode.getOutPort().name}
+                   data-nodeid={diagramNode.getOutPort().parent.id}/>
             </div>
           </div>
-          ) : (
+        ) : (
           <div>
             <div className="py-2 node-label">
               <div className="col text-center">
-                {diagramNode.node.label_translations['en']}
+                {diagramNode.node.label_translations["en"]}
               </div>
             </div>
             <div className="node-answers">
-                {outPorts}
+              {outPorts}
             </div>
           </div>
-          )}
+        )}
+        {diagramNode.node.reference === "DF7" ? (
+          <div>
+            <div className="inExcluded"/>
+            <div className="outExcluded"/>
+          </div>
+        ) : null}
       </div>
     );
   }
