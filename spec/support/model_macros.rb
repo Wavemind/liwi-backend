177ef2b5830493e8727da0c8d6676264210b79ad
Quentin Girard <quentin.girard@wavemind.ch>
2019-05-07 15:13:54 +0200

    Add condition in macro for rspec

--- spec/support/model_macros.rb
+++ spec/support/model_macros.rb
@@ -80,6 +80,9 @@ module ModelMacros
       @dd7_s2 = Instance.create!(instanceable: @dd7, node: s2)
       @dd7_df7 = Instance.create!(instanceable: @dd7, node: @df7)
 
+      # Conditions
+      Condition.create!(referenceable: @dd7_df7, first_conditionable: @p3_2, top_level: true)
+
       # PS
       ps_category = Category.create!(reference_prefix: 'PS', name_en: 'Predefined syndrome', parent: 'PredefinedSyndrome')
       ps6 = PredefinedSyndrome.create!(algorithm: @algorithm, reference: '6', label_en: 'Able to drink', category: ps_category)
