f5cf694af815bae6fda90a6563732a3d30964b0d
Quentin Girard <quentin.girard@wavemind.ch>
2019-07-01 08:26:38 +0200

    Remove available for FTA

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:is_available]
+  before_action :authenticate_user!
 
   def is_available
-    render json: 'true'
+    render json: 'false'
   end
 
   protected
