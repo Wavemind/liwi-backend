52c2840f49916db88457e8d75cde138339c4c287
Quentin Girard <quentin.girard@wavemind.ch>
2019-07-12 10:21:12 +0200

    Improve toolbar

--- app/javascript/components/utils/Toolbar.js
+++ app/javascript/components/utils/Toolbar.js
@@ -17,29 +17,29 @@ class Toolbar extends React.Component {
   // Button to open modal with new final diagnostic node form
   newFinalDiagnostic = () => {
     const { set } = this.props;
-    set('modalToOpen', 'CreateFinalDiagnostic');
-    set('modalIsOpen', true);
+    set("modalToOpen", "CreateFinalDiagnostic");
+    set("modalIsOpen", true);
   };
 
   // Button to open modal with new predefined syndrome node form
   newQuestionsSequence = () => {
     const { set } = this.props;
-    set('modalToOpen', 'CreateQuestionsSequence');
-    set('modalIsOpen', true);
+    set("modalToOpen", "CreateQuestionsSequence");
+    set("modalIsOpen", true);
   };
 
   // Button to open modal with new predefined syndrome node form
   newQuestion = () => {
     const { set } = this.props;
-    set('modalToOpen', 'CreateQuestion');
-    set('modalIsOpen', true);
+    set("modalToOpen", "CreateQuestion");
+    set("modalIsOpen", true);
   };
 
   newHealthCare = (type) => {
     const { set } = this.props;
-    set('currentHealthCareType', type);
-    set('modalToOpen', 'CreateHealthCare');
-    set('modalIsOpen', true);
+    set("currentHealthCareType", type);
+    set("modalToOpen", "CreateHealthCare");
+    set("modalIsOpen", true);
   };
 
   // Launch validation of diagram and display flash message
@@ -48,13 +48,12 @@ class Toolbar extends React.Component {
       http,
       addMessage,
       type,
-      instanceable
     } = this.props;
 
     let response;
-    this.setState({isLoading: true});
+    this.setState({ isLoading: true });
 
-    if (type === 'Diagnostic') {
+    if (type === "Diagnostic") {
       response = await http.validateDiagnostic();
     } else {
       response = await http.validateQuestionsSequence();
@@ -62,10 +61,10 @@ class Toolbar extends React.Component {
 
     let message = {
       status: response.status,
-      messages: response.messages,
+      messages: response.messages
     };
     await addMessage(message);
-    this.setState({isLoading: false})
+    this.setState({ isLoading: false });
   };
 
   redirectToDiagnostic = () => {
@@ -76,17 +75,17 @@ class Toolbar extends React.Component {
   // Close and redirect user to list of...
   save = async () => {
     const { http, type, instanceable } = this.props;
-    if (type === 'Diagnostic') {
+    if (type === "Diagnostic") {
       await http.redirectToDiagnostic();
     } else {
-      let panel = instanceable.category_name === 'scored' ? 'questions_sequences_scored' : 'questions_sequences';
+      let panel = instanceable.category_name === "scored" ? "questions_sequences_scored" : "questions_sequences";
       await http.redirectToAlgorithm(panel);
     }
   };
 
   render() {
-    const { isLoading } = this.state;
     const { type } = this.props;
+    const { isLoading } = this.state;
 
     return (
       <div className="col-md-12 liwi-toolbar">
@@ -98,28 +97,31 @@ class Toolbar extends React.Component {
                 New
               </button>
               <div className="dropdown-menu">
-                <a className="dropdown-item" href="#" onClick={() => {this.newQuestion()}}>Question</a>
-                <a className="dropdown-item" href="#" onClick={() => {this.newQuestionsSequence()}}>Questions Sequence</a>
-                {type === 'Diagnostic' ? (<a className="dropdown-item" href="#" onClick={() => {this.newFinalDiagnostic()}}>Final diagnostic</a>) : null}
-                {type === 'FinalDiagnostic' ? (<a className="dropdown-item" href="#" onClick={() => {this.newHealthCare('treatments')}}>Treatment</a>) : null}
-                {type === 'FinalDiagnostic' ? (<a className="dropdown-item" href="#" onClick={() => {this.newHealthCare('managements')}}>Management</a>) : null}
+                <a className="dropdown-item" href="#" onClick={this.newQuestion}>Question</a>
+                <a className="dropdown-item" href="#" onClick={this.newQuestionsSequence}>Questions Sequence</a>
+                {type === "Diagnostic" ? (
+                  <a className="dropdown-item" href="#" onClick={this.newFinalDiagnostic}>Final diagnostic</a>) : null}
+                {type === "FinalDiagnostic" ? (<a className="dropdown-item" href="#"
+                                                  onClick={() => this.newHealthCare("treatments")}>Treatment</a>) : null}
+                {type === "FinalDiagnostic" ? (<a className="dropdown-item" href="#"
+                                                  onClick={() => this.newHealthCare("managements")}>Management</a>) : null}
               </div>
             </div>
           </div>
           <div className="col text-right">
-            {type === 'Diagnostic' || type === "QuestionsSequence" ? (
-            <button type="button" className="btn btn-transparent" onClick={() => {this.validate()}}>
-              {isLoading ?
-                <span>Loading</span>: <span>Validate</span>
-              }
-            </button>
-              ) : null }
-            {type === 'FinalDiagnostic' ? (
-              <button type="button" className="btn btn-transparent" onClick={() => {this.redirectToDiagnostic()}}>
+            {type === "Diagnostic" || type === "QuestionsSequence" ? (
+              <button type="button" className="btn btn-transparent" onClick={this.validate}>
+                {isLoading ?
+                  <span>Loading</span> : <span>Validate</span>
+                }
+              </button>
+            ) : null}
+            {type === "FinalDiagnostic" ? (
+              <button type="button" className="btn btn-transparent" onClick={this.redirectToDiagnostic}>
                 Diagnostic diagram
               </button>
             ) : (
-              <button type="button" className="btn btn-transparent" onClick={() => {this.save()}}>
+              <button type="button" className="btn btn-transparent" onClick={this.save}>
                 Save
               </button>
             )}
