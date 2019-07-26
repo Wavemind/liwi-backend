d0effb9a4a4038d04afc306dabe14d1e5f655b47
Emmanuel <emmanuel.barchichat@wavemind.ch>
2019-07-26 07:42:52 +0000

    Revert "Create specs for the formula"
    This reverts commit 14615bc064c438212616faa0c6a5190587b3dd3d.

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -37,43 +37,4 @@ RSpec.describe Question, type: :model do
 
     expect(question.answers.count).to eq(2)
   end
-
-  context 'it validates formula correctly' do
-    before(:each) do
-      @question = Questions::Symptom.new(reference: '9', label: 'skin issue', priority: Question.priorities[:basic], answer_type: @answer_type, algorithm: @algorithm)
-    end
-
-    it 'Pass through with a correct formula' do
-      @question.formula = "8 * 5"
-
-      expect(@question).to be_valid
-    end
-
-    it 'Refuses incorrect characters' do
-      @question.formula = "8 * 5 + x"
-
-      expect(@question).to_not be_valid
-    end
-
-    it 'Pass through with a correct formula within a reference' do
-      integer = AnswerType.create!(value: 'Integer', display: 'Input')
-      Questions::Symptom.create!(reference: '10', label: 'skin issue', priority: Question.priorities[:basic], answer_type: integer, algorithm: @algorithm)
-
-      @question.formula = "[S10] * 10"
-      expect(@question).to be_valid
-    end
-
-    it 'Refuses if any reference is wrong' do
-      @question.formula = "[W10] * 10"
-
-      expect(@question).to_not be_valid
-    end
-
-    it 'Refuses if the reference used is not a numeric format' do
-      Questions::Symptom.create!(reference: '10', label: 'skin issue', priority: Question.priorities[:basic], answer_type: @answer_type, algorithm: @algorithm)
-      @question.formula = "[S10] * 10"
-
-      expect(@question).to_not be_valid
-    end
-  end
 end
