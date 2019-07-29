a60433deec67f606fee392b6aeea874642b96199
Emmanuel <emmanuel.barchichat@wavemind.ch>
2019-07-29 09:24:49 +0000

    Hotfix for the qs edit

--- app/datatables/questions_sequence_datatable.rb
+++ app/datatables/questions_sequence_datatable.rb
@@ -24,7 +24,7 @@ class QuestionsSequenceDatatable < AjaxDatatablesRails::ActiveRecord
   # Value display
   def data
     records.map do |record|
-      actions = link_to(I18n.t('show'), diagram_questions_sequence_url(record), class: 'btn btn-outline-primary') + " " + link_to(I18n.t('edit'), edit_algorithm_questions_sequence_url(params[:id], id: record), class: 'btn btn-outline-info') + " " + link_to(I18n.t('delete'), algorithm_questions_sequence_url(record.algorithm, record), class: "btn btn-outline-danger #{record.dependencies? ? 'disabled' : ''}", method: :delete, data: { confirm: 'Are you sure?' })
+      actions = link_to(I18n.t('show'), diagram_questions_sequence_url(record), class: 'btn btn-outline-primary') + " " + link_to(I18n.t('edit'), edit_algorithm_questions_sequence_url(record.algorithm, record), class: 'btn btn-outline-info') + " " + link_to(I18n.t('delete'), algorithm_questions_sequence_url(record.algorithm, record), class: "btn btn-outline-danger #{record.dependencies? ? 'disabled' : ''}", method: :delete, data: { confirm: 'Are you sure?' })
       {
         reference: record.reference,
         category: Object.const_get(record.type).display_label,
