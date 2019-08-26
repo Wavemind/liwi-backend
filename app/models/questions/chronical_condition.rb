8db1998e9c73d47a19ac33195c48dffb2d4c8e0e
Emmanuel <emmanuel.barchichat@wavemind.ch>
2019-08-26 09:44:06 +0000

    Add both categories first look assessment and chronical condition

---
+++ app/models/questions/chronical_condition.rb
@@ -0,0 +1,8 @@
+# Category of question who defines other condition that might influence the present patient condition
+class Questions::ChronicalCondition < Question
+
+  def self.variable
+    'chronical_condition'
+  end
+
+end
---
+++ app/models/questions/first_look_assessment.rb
@@ -0,0 +1,8 @@
+# Category of question who defines first basic question to possibly improve priority of a patient
+class Questions::FirstLookAssessment < Question
+
+  def self.variable
+    'first_look_assessment'
+  end
+
+end
