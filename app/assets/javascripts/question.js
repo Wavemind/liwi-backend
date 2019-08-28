45ce81b40a89a002b96ee10cb5280013308c9a13
Emmanuel <emmanuel.barchichat@wavemind.ch>
2019-08-28 04:22:08 +0000

    Force the stage triage for chronical conditions, chief complaints, vital signs and chronical conditions

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -45,10 +45,17 @@ jQuery(document).ready(function() {
           } else if(response.responseText !== "A") {
             $(questionUnavailable).addClass("d-none");
           }
+
           if (response.responseText === "CC" || response.responseText === "V") {
             $("#question_answer_type_id").val("1").attr("disabled", true);
           } else {
             $("#question_answer_type_id").attr("disabled", false);
+          }
+
+          if (response.responseText === "CC" || response.responseText === "VS" || response.responseText === "CH" || response.responseText === "FL") {
+            $("#question_stage").val("triage").attr("disabled", true);
+          } else {
+            $("#question_stage  ").attr("disabled", false);
           }
         }
       });
