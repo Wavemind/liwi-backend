e18d255a5b64c0ad9167cb8b74faaacb5a7ff4a0
Alain Fresco <alain.fresco@gmail.com>
2019-11-11 13:35:32 +0100

    Remove user auth for api testing purpose

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:is_available]
+  #before_action :authenticate_user!, except: [:is_available]
 
   def is_available
     render json: 'true'
