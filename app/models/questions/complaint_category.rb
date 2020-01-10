1a678a1f131eaf360cd577a5dd6160239f215e5f
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-01-10 10:06:34 +0000

    Fix to prevent user to remove complaint category question if it is used by a diagnostic

--- app/models/questions/complaint_category.rb
+++ app/models/questions/complaint_category.rb
@@ -2,8 +2,15 @@
 # Reference prefix : CC
 class Questions::ComplaintCategory < Question
 
+
   def self.variable
     'complaint_category'
+  end
+
+  # @return [Boolean]
+  # Verify if current node have instances dependencies
+  def instance_dependencies?
+    dependencies.map(&:instanceable).present? || diagnostics.any?
   end
 
 end
