b2eb525a0d496ff559a1d53788b10191c8f1604a
Quentin Girard <quentin.girard@wavemind.ch>
2020-03-20 10:15:11 +0100

    Add translation for toolbar

--- app/javascript/apps/diagram/components/Toolbar/index.js
+++ app/javascript/apps/diagram/components/Toolbar/index.js
@@ -31,33 +31,33 @@ class Toolbar extends React.Component {
         <div className="row">
           <div className="col">
             <div className="btn-group">
-              <button type="button" className="btn btn-transparent" data-toggle="dropdown"
+              <button key="new" type="button" className="btn btn-transparent" data-toggle="dropdown"
                       aria-haspopup="true" aria-expanded="false">
-                New
+                {I18n.t("toolbar.new")}
               </button>
               <div className="dropdown-menu">
-                <a className="dropdown-item" href="#">Question</a>
-                <a className="dropdown-item" href="#">Questions Sequence</a>
-                {instanceable.type === "Diagnostic" ? (<a className="dropdown-item" href="#" onClick={() => this.createNode(I18n.t("final_diagnostics.new.title"), 'FinalDiagnosticForm')}>Final diagnostic</a>) : null}
-                {instanceable.type === "FinalDiagnostic" ? (<a className="dropdown-item" href="#">Treatment</a>) : null}
-                {instanceable.type === "FinalDiagnostic" ? (<a className="dropdown-item" href="#">Management</a>) : null}
+                <a className="dropdown-item" key="questions" href="#">{I18n.t("toolbar.question")}</a>
+                <a className="dropdown-item" key="questionsSequence" href="#">{I18n.t("toolbar.questions_sequence")}</a>
+                {instanceable.type === "Diagnostic" ? (<a className="dropdown-item" key="finalDiagnostic" href="#" onClick={() => this.createNode(I18n.t("final_diagnostics.new.title"), 'FinalDiagnosticForm')}>{I18n.t("toolbar.final_diagnostic")}</a>) : null}
+                {instanceable.type === "FinalDiagnostic" ? (<a className="dropdown-item" key="treatment" href="#">{I18n.t("toolbar.treatment")}</a>) : null}
+                {instanceable.type === "FinalDiagnostic" ? (<a className="dropdown-item" key="management" href="#">{I18n.t("toolbar.management")}</a>) : null}
               </div>
             </div>
           </div>
 
           <div className="col text-right">
             {instanceable.type === "Diagnostic" || instanceable.type === "QuestionsSequence" ? (
-                <button type="button" className="btn btn-transparent">
-                  <span>Validate</span>
+                <button key="validate" type="button" className="btn btn-transparent">
+                  <span>{I18n.t("toolbar.validate")}</span>
                 </button>
             ) : null}
             {instanceable.type === "FinalDiagnostic" ? (
-              <button type="button" className="btn btn-transparent">
-                Diagnostic diagram
+              <button key="diagnosticDiagram" type="button" className="btn btn-transparent">
+                {I18n.t("toolbar.diagnostic_diagram")}
               </button>
             ) : (
-              <button type="button" className="btn btn-transparent">
-                Save
+              <button key="save" type="button" className="btn btn-transparent">
+                {I18n.t("toolbar.save")}
               </button>
             )}
           </div>
