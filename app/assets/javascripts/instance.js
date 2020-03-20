ee1f864f06373f6a5ea5a4dc2be5ca8feb65b8c7
Alain Fresco <alain.fresco@gmail.com>
2020-03-20 14:56:38 +0100

    Codacy

--- app/assets/javascripts/instance.js
+++ app/assets/javascripts/instance.js
@@ -1,21 +1,23 @@
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
   $("#instances-datatable").dataTable({
-    "processing": true,
-    "info": false,
-    "bLengthChange": false,
-    "serverSide": true,
-    "ajax": $("#instances-datatable").data("source"),
-    "pagingType": "full_numbers",
-    "columns": [
-      { "data": "reference" },
-      { "data": "type" },
-      { "data": "label" },
-      { "data": "children" },
-      { "data": "actions", "className": "text-right" },
+    processing: true,
+    info: false,
+    bLengthChange: false,
+    serverSide: true,
+    ajax: $("#instances-datatable").data("source"),
+    pagingType: "full_numbers",
+    columns: [
+      { data: "reference" },
+      { data: "type" },
+      { data: "label" },
+      { data: "children" },
+      { data: "actions", className: "text-right" }
     ],
-    'columnDefs': [ {
-      'targets': [4],
-      'orderable': false,
-    }]
+    columnDefs: [
+      {
+        targets: [4],
+        orderable: false
+      }
+    ]
   });
 });
