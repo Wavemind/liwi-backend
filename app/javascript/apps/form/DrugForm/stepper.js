a1a0d35e85d51439406df6a8a27d93799336f465
Quentin Girard <quentin.girard@wavemind.ch>
2020-03-31 10:21:50 +0200

    Add step 3

--- app/javascript/apps/form/DrugForm/stepper.js
+++ app/javascript/apps/form/DrugForm/stepper.js
@@ -1,9 +1,6 @@
 import * as React from "react";
 
 import Http from "../../diagram/engine/http";
-import store from "../../diagram/engine/reducers/store";
-import { closeModal } from "../../diagram/engine/reducers/creators.actions";
-import { createNode } from "../../diagram/helpers/nodeHelpers";
 import DrugForm from "./drugForm";
 import FormulationForm from "./formulationForm";
 import DisplayErrors from "../components/DisplayErrors";
@@ -17,7 +14,8 @@ export default class StepperDrugForm extends React.Component {
     this.state = {
       errors: null,
       step: 1,
-      drug: this.drugBody(drug, method)
+      drug: this.drugBody(drug, method),
+      createdDrug: {}
     };
   }
 
@@ -38,12 +36,10 @@ export default class StepperDrugForm extends React.Component {
    * Send value to server
    */
   save = async () => {
-    const { method, from, engine, diagramObject, addAvailableNode } = this.props;
+    const { method, from, diagramObject } = this.props;
     const { drug } = this.state;
     let http = new Http();
     let httpRequest = {};
-
-    console.log(drug)
 
     if (method === "create") {
       httpRequest = await http.createDrug(drug, from);
@@ -58,17 +54,10 @@ export default class StepperDrugForm extends React.Component {
         window.location.replace(result.url);
       } else {
         if (method === "create") {
-          let diagramInstance = createNode(result, addAvailableNode, false, "Diagnostic", engine);
-          engine.getModel().addNode(diagramInstance);
+          this.setState({step: 3, createdDrug: result.drug})
         } else {
           diagramObject.options.dbInstance.node = result;
         }
-
-        engine.repaintCanvas();
-
-        store.dispatch(
-          closeModal()
-        );
       }
     } else {
       this.setState({ errors: result });
@@ -109,8 +98,8 @@ export default class StepperDrugForm extends React.Component {
   };
 
   render() {
-    const { errors, step, drug } = this.state;
-    const { method } = this.props;
+    const { errors, step, drug, createdDrug } = this.state;
+    const { method, engine, diagramObject, addAvailableNode } = this.props;
 
     switch (step) {
       case 1:
@@ -134,6 +123,16 @@ export default class StepperDrugForm extends React.Component {
             />
           </>
         );
+    case 3:
+      return (
+        <FormulationForm
+          engine={engine}
+          diagramObject={diagramObject}
+          addAvailableNode={addAvailableNode}
+          method={method}
+          drug={createdDrug}
+        />
+      );
       default:
         return "boom boom";
     }
