b0e5bcabcbff16f0a80d1390e1c5c4e69b537374
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-06-17 06:52:48 +0000

    Test appsignal error

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    begin
+        raise
+    rescue => e
+      Appsignal.send_error(e, {algorithm: 'Hi there'})
+    end
   end
 end
