ef373b17323a63458f0cfa414cf636f892fc6e9c
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-06-17 07:41:58 +0000

    Remove test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,5 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    begin
-        raise
-    rescue => e
-      Appsignal.send_error(e, {algorithm: 'Hi there'})
-    end
   end
 end
