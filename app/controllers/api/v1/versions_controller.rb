b0c1f0cfe87afd095be13869269fc77abc28b5b5
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-06-19 14:05:47 +0000

    Fix api errors

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -13,16 +13,16 @@ class Api::V1::VersionsController < Api::V1::ApplicationController
           if version.present?
             render json: VersionsService.generate_version_hash(version.id)
           else
-            render json: { error: t('.no_version') }, status: :unprocessable_entity
+            render json: { errors: t('.no_version') }, status: :unprocessable_entity
           end
         else
-          render json:  { error: t('.invalid_token') }, status: :unprocessable_entity
+          render json:  { errors: t('.invalid_token') }, status: :unprocessable_entity
         end
       else
-        render json: { error: t('.no_group') }, status: :unprocessable_entity
+        render json: { errors: t('.no_group') }, status: :unprocessable_entity
       end
     else
-      render json: { error: t('.device_not_exist') }, status: :unprocessable_entity
+      render json: { errors: t('.device_not_exist') }, status: :unprocessable_entity
     end
   end
 end
