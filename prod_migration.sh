7a7a211c46dd1394cfd02e0cfefe33228ea80e6d
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-07-29 07:50:34 +0000

    Update script

--- prod_migration.sh
+++ prod_migration.sh
@@ -1,20 +1,21 @@
 echo "========================================================"
-
 dokku maintenance:on medalc
 dokku maintenance:on liwi-test
 echo "Maintenance enable"
-
-dokku postgres:unlink liwi-test
+echo "======================================================="
+dokku postgres:unlink liwi-test liwi-test
+echo "Database unlink"
+echo "======================================================="
 dokku postgres:destroy liwi-test
+echo "Database destroy"
+echo "======================================================="
 dokku postgres:clone medalc liwi-test
 dokku postgres:link liwi-test liwi-test
 echo "Clone finish"
-
+echo "======================================================="
 dokku run liwi-test rake users:seed_clinicians
 echo "Seeds done"
-
 dokku maintenance:off medalc
 dokku maintenance:off liwi-test
 echo "Maintenance disable"
-
 echo "Finish"
