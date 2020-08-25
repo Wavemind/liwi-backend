6a593267e19e175b8016c1cc7e7ff622921b5963
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-08-25 10:12:20 +0000

    Hotfix preventing questions in final diagnosis diagram to override the same question asked in the diagnosis

--- app/services/versions_service.rb
+++ app/services/versions_service.rb
@@ -196,11 +196,8 @@ class VersionsService
       # Append the questions in order to list them all at the end of the json.
       assign_node(question_instance.node)
 
-      if question_instance.final_diagnostic_id.nil?
-        hash['instances'][question_instance.node.id] = extract_instances(question_instance)
-      else
-        hash['final_diagnostics'][question_instance.final_diagnostic_id]['instances'][question_instance.node.id] = hash['instances'][question_instance.node.id]
-      end
+      hash['instances'][question_instance.node.id] = extract_instances(question_instance) if hash['instances'][question_instance.node.id] || question_instance.final_diagnostic_id.nil?
+      hash['final_diagnostics'][question_instance.final_diagnostic_id]['instances'][question_instance.node.id] = hash['instances'][question_instance.node.id] unless question_instance.final_diagnostic_id.nil?
     end
 
     # Loop in each predefined syndromes used in current diagnostic
@@ -208,11 +205,8 @@ class VersionsService
       # Append the predefined syndromes in order to list them all at the end of the json.
       assign_node(questions_sequence_instance.node)
 
-      if questions_sequence_instance.final_diagnostic_id.nil?
-        hash['instances'][questions_sequence_instance.node.id] = extract_instances(questions_sequence_instance)
-      else
-        hash['final_diagnostics'][questions_sequence_instance.final_diagnostic_id]['instances'][questions_sequence_instance.node.id] = hash['instances'][questions_sequence_instance.node.id]
-      end
+      hash['instances'][questions_sequence_instance.node.id] = extract_instances(questions_sequence_instance) if hash['instances'][questions_sequence_instance.node.id] || questions_sequence_instance.final_diagnostic_id.nil?
+      hash['final_diagnostics'][questions_sequence_instance.final_diagnostic_id]['instances'][questions_sequence_instance.node.id] = hash['instances'][questions_sequence_instance.node.id] unless questions_sequence_instance.final_diagnostic_id.nil?
     end
 
     hash
