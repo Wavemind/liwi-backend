4648d22ae7f0c9ead2d967b474649a6cdd3c2ff7
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-09-07 13:23:40 +0000

    Put medias on json and update uploader to make the amazon url

--- app/services/versions_service.rb
+++ app/services/versions_service.rb
@@ -376,6 +376,8 @@ class VersionsService
       hash[question.id]['max_message_error'] = question.max_message_error
       hash[question.id]['diagnostics_related_to_cc'] = get_complaint_category_diagnostics(question, []) if question.is_a?(Questions::ComplaintCategory)
 
+      hash[question.id]['urls'] = question.medias.map(&:url).map(&:url)
+
       question.answers.each do |answer|
         answer_hash = {}
         answer_hash['id'] = answer.id
