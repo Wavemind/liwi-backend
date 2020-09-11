1e67b75c051c0d03a91c0f291348a665cc6d5267
Quentin Girard <quentin.girard@wavemind.ch>
2020-09-11 14:33:26 +0200

    Add version policy

---
+++ app/policies/version_policy.rb
@@ -0,0 +1,83 @@
+class VersionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    user.admin? || user.clinician? || user.deployment_manager?
+  end
+
+  def show?
+    index?
+  end
+
+  def new?
+    user.admin? || user.clinician?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    new?
+  end
+
+  def update?
+    new?
+  end
+
+  def archive?
+    new?
+  end
+
+  def unarchive?
+    new?
+  end
+
+  def change_triage_order?
+    new?
+  end
+
+  def components?
+    new?
+  end
+
+  def remove_components?
+    new?
+  end
+
+  def create_triage_condition?
+    new?
+  end
+
+  def duplicate?
+    new?
+  end
+
+  def final_diagnostics?
+    index?
+  end
+
+  def final_diagnoses_exclusions?
+    index?
+  end
+
+  def generate_variables?
+    new?
+  end
+
+  def regenerate_json?
+    index?
+  end
+
+  def remove_triage_condition?
+    new?
+  end
+
+  def update_list?
+    new?
+  end
+end
