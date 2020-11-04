06b14443573e632341d89f8022e8bd7e15698ead
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-11-04 15:35:41 +0000

    Add comments and remove puts from PR

--- app/models/questions_sequence.rb
+++ app/models/questions_sequence.rb
@@ -169,19 +169,15 @@ class QuestionsSequence < Node
     nodes
   end
 
+  # Check if the user is doing a loop between multiple QS
   def self.is_loop(qs_diagram, qs_node)
     parents = QuestionsSequence.get_qs_parents(qs_diagram, [])
-    puts '**'
-    puts parents.inspect
-    puts '**'
     parents.include? qs_node.id
   end
 
+  # Check recursively the QS parents 
   def self.get_qs_parents(qs, parents)
     qs.instances.map do |instance|
-      puts '***'
-      puts instance.inspect
-      puts '***'
       if instance.instanceable_type == 'Node' && instance.instanceable_id != instance.node_id
         parents.push(instance.instanceable_id)
         parents = QuestionsSequence.get_qs_parents(instance.instanceable, parents)
