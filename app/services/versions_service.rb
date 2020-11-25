c719348d5a9be3c2504ce1b1720a9c5dcbb34af3
Emmanuel <emmanuel.barchichat@wavemind.ch>
2020-11-25 13:33:26 +0000

    Fix key name system_order

--- app/services/versions_service.rb
+++ app/services/versions_service.rb
@@ -141,7 +141,7 @@ class VersionsService
     hash['second_top_right_question_id'] = @version.second_top_right_question.present? ? @version.second_top_right_question.node_id : nil
 
     hash['questions_orders'] = @version.medal_r_config['questions_orders']
-    hash['systems_orders'] = @version.medal_r_config['systems_orders']
+    hash['systems_order'] = @version.medal_r_config['systems_order']
     hash['medical_case_list'] = @version.medal_r_config['medical_case_list_order']
     hash['patient_list'] = @version.medal_r_config['patient_list_order']
     hash
