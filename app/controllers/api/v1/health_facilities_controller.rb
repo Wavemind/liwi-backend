1c9363c025bbc998fcfd600824cdf66f31a14d33
Sinan Ucak <ucak.sinan@gmail.com>
2021-01-04 15:58:39 +0100

    corrected Quentin's comment

--- app/controllers/api/v1/health_facilities_controller.rb
+++ app/controllers/api/v1/health_facilities_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::HealthFacilitiesController < Api::V1::ApplicationController
   def show
     health_facility = HealthFacility.find_by_id(params[:id])
-    if health_facility
+    if health_facility.present?
       render json: health_facility
     else
       render json: { errors: t('api.v1.health_facilities.show.no_health_facility') }, status: :unprocessable_entity
