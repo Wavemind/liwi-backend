a4b794cd187b35801cefc37264abf06f2182e368
Sinan Ucak <ucak.sinan@gmail.com>
2021-01-05 13:17:10 +0100

    PR comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,7 +86,5 @@ Rails.application.configure do
     Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Condition', association: :first_conditionable
     Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Condition', association: :second_conditionable
   end
-
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 end
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
