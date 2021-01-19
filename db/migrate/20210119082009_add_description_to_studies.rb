260aeeef5e5f0f126f6d3c304cd2fea30e51dfbd
Quentin Girard <quentin.girard@wavemind.ch>
2021-01-19 12:01:52 +0100

    Remove duplicated migration

--- db/migrate/20210119082009_add_description_to_studies.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToStudies < ActiveRecord::Migration[5.2]
-  def change
-    add_column :studies, :description, :text, default: ""
-  end
-end
--- db/migrate/20210119082040_add_emergency_to_algorithm.rb
+++
@@ -1,5 +0,0 @@
-class AddEmergencyToAlgorithm < ActiveRecord::Migration[5.2]
-  def change
-    add_column :algorithms, :emergency_content, :text, default: ""
-  end
-end
