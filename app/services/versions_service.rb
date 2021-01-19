7976d089b0be94b4624ce387d2e223c30eb68e72
Sinan Ucak <ucak.sinan@gmail.com>
2021-01-19 11:58:36 +0100

    changed emergency_content name in versions service

--- app/services/versions_service.rb
+++ app/services/versions_service.rb
@@ -113,7 +113,7 @@ class VersionsService
     hash = {}
     hash['version_id'] = @version.id
     hash['version_name'] = @version.name
-    hash['algorithm_emergency_content'] = @version.algorithm.emergency_content
+    hash['emergency_content'] = @version.algorithm.emergency_content
     hash['json_version'] = @version.medal_r_json_version
     hash['description'] = @version.description
     hash['algorithm_id'] = @version.algorithm.id
