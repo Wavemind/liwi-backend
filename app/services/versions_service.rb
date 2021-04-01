f09597618d06c0080d3644f62a05cf093fa16c75
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-04-01 12:15:40 +0000

    Send empty hash instead of nil if value is nil

--- app/services/versions_service.rb
+++ app/services/versions_service.rb
@@ -49,7 +49,7 @@ class VersionsService
   private
 
   def self.return_hstore_translated(field)
-    return nil if field.nil?
+    return Hash[@available_languages.collect { |k| [k, ""] } ] if field.nil?
     field.select {|k,v| @available_languages.include?(k)}
   end
 
