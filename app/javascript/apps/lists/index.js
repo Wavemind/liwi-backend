46eff50c46265da8c697259164a0b9a2d7dbef81
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-06-23 16:16:42 +0000

    Add search bar

--- app/javascript/apps/lists/index.js
+++ app/javascript/apps/lists/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, Dropdown} from "react-bootstrap";
+import {Card, Dropdown, Form, InputGroup} from "react-bootstrap";
 import I18n from "i18n-js";
 
 const CustomToggle = React.forwardRef(({children, onClick}, ref) => (
@@ -22,6 +22,7 @@ class ListsComponent extends Component {
     super(props);
     const { list } = this.props;
     this.state = {
+      fullList: list,
       list: list,
       index: window.location.href,
       generating: false,
@@ -99,12 +100,12 @@ class ListsComponent extends Component {
             console.error(error);
           });
         const newList = await response.json();
-        console.log(newList)
+
         this.setState({
           generating: false,
           message: I18n.t(`job_status.duplicate_${data.job_status}`),
           alert_type: 'success',
-          list: newList
+          fullList: newList
         })
       } else {
         this.setState({
@@ -122,7 +123,6 @@ class ListsComponent extends Component {
     const data = await response.json();
 
     if (data.job_id) { // Duplicate with job
-
       if (data.success) {
         this.setState({
           current_duplicated_version: object.id,
@@ -147,12 +147,22 @@ class ListsComponent extends Component {
 
   handleDelete = async (object, show) => {
     const {index} = this.state;
-    // TODO handle warning message
     if (window.confirm('Are you sure ?')) {
       const header = this.setHeader({}, "DELETE");
       await fetch(show, header).catch(error => console.log(error));
 
       window.location.href = index;
+    }
+  };
+
+  handleSearch = (val) => {
+    const { title } = this.props;
+    const { fullList } = this.state;
+    if (val.target.value === "") {
+      this.setState({list: fullList})
+    } else {
+      const newList = fullList.filter(data => this.displayText(data, title).toUpperCase().includes(val.target.value.toUpperCase()));
+      this.setState({list: newList})
     }
   };
 
@@ -186,57 +196,70 @@ class ListsComponent extends Component {
         <div className="col">
           {this.renderMessage()}
         </div>
+        <div className="col-3 offset-9">
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>&#x1F50E;&#xFE0E;</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              name="search"
+              onChange={(val) => {this.handleSearch(val)}}
+            />
+          </InputGroup>
+        </div>
         {list.map(object => (
-          <Card key={object["id"]}>
-            <Card.Body>
-              <Card.Title>
-                <span>
-                  <Dropdown drop="left">
-                    <Dropdown.Toggle as={CustomToggle} id="dropdown-basic"></Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      {actions['diagram'] ?
-                        <Dropdown.Item href={`${this.replaceIdInUrl(show, object["id"])}/diagram`}>{I18n.t('open_diagram')}</Dropdown.Item>
-                      : null}
+          <div className="col">
+            <Card key={object["id"]}>
+              <Card.Body>
+                <Card.Title>
+                  <span>
+                    <Dropdown drop="left">
+                      <Dropdown.Toggle as={CustomToggle} id="dropdown-basic"></Dropdown.Toggle>
+                      <Dropdown.Menu>
+                        {actions['diagram'] ?
+                          <Dropdown.Item href={`${this.replaceIdInUrl(show, object["id"])}/diagram`}>{I18n.t('open_diagram')}</Dropdown.Item>
+                        : null}
 
-                      {actions['edit'] ?
-                        <Dropdown.Item href={`${this.replaceIdInUrl(show, object["id"])}/edit`}>{I18n.t('edit')}</Dropdown.Item>
-                      : null}
+                        {actions['edit'] ?
+                          <Dropdown.Item href={`${this.replaceIdInUrl(show, object["id"])}/edit`}>{I18n.t('edit')}</Dropdown.Item>
+                        : null}
 
-                      {actions['duplicate'] ?
-                        <Dropdown.Item onClick={() => this.handleDuplicate(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('duplicate')}</Dropdown.Item>
-                      : null}
+                        {actions['duplicate'] ?
+                          <Dropdown.Item onClick={() => this.handleDuplicate(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('duplicate')}</Dropdown.Item>
+                        : null}
 
-                      {actions['archive'] ?
-                        (object['archived'] ?
-                          <Dropdown.Item onClick={() => this.handleUnarchive(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('unarchive')}</Dropdown.Item>
-                          :
-                          <Dropdown.Item onClick={() => this.handleArchive(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('archive')}</Dropdown.Item>
-                        )
-                      : null}
+                        {actions['archive'] ?
+                          (object['archived'] ?
+                            <Dropdown.Item onClick={() => this.handleUnarchive(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('unarchive')}</Dropdown.Item>
+                            :
+                            <Dropdown.Item onClick={() => this.handleArchive(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('archive')}</Dropdown.Item>
+                          )
+                        : null}
 
-                      {actions['delete'] ?
-                        <Dropdown.Item onClick={() => this.handleDelete(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('delete')}</Dropdown.Item>
-                      : null}
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </span>
-                <span style={{cursor: 'pointer'}} onClick={() => window.location.href = this.replaceIdInUrl(show, object.id)}>
-                  {this.displayText(object, title)}
-                </span>
-                {metadata.map(data => (
-                  <p key={data} className="metadata-list" dangerouslySetInnerHTML={{__html: object[data]}}/>
-                ))}
-              </Card.Title>
-              <Card.Text>
-                {this.displayText(object, description)}
-              </Card.Text>
-              <Card.Text className="text-muted">
-                <span className="list-updated-at">
-                  Last updated the {this.formatDate(object["updated_at"].substring(0, 10))}
-                </span>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+                        {actions['delete'] ?
+                          <Dropdown.Item onClick={() => this.handleDelete(object, this.replaceIdInUrl(show, object.id))}>{I18n.t('delete')}</Dropdown.Item>
+                        : null}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </span>
+                  <span style={{cursor: 'pointer'}} onClick={() => window.location.href = this.replaceIdInUrl(show, object.id)}>
+                    {this.displayText(object, title)}
+                  </span>
+                  {metadata.map(data => (
+                    <p key={data} className="metadata-list" dangerouslySetInnerHTML={{__html: object[data]}}/>
+                  ))}
+                </Card.Title>
+                <Card.Text>
+                  {this.displayText(object, description)}
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  <span className="list-updated-at">
+                    Last updated the {this.formatDate(object["updated_at"].substring(0, 10))}
+                  </span>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
         ))}
       </div>
     );
