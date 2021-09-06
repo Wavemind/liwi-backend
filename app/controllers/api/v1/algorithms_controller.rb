d75beeda3caae3da2ccef23cb3cf64e86d8fa835
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-09-06 14:29:31 +0000

    Route for emergency_content with a version number

--- app/controllers/api/v1/algorithms_controller.rb
+++ app/controllers/api/v1/algorithms_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::AlgorithmsController < Api::V1::ApplicationController
     render json: Algorithm.all.select(:id, :name, :archived, :created_at, :updated_at)
   end
 
+  # PUT /algorithms/:id/emergency_content
+  # @params algorithm_id [Integer]
+  # Send emergency_content if the version is not the same
+  def emergency_content
+    algorithm = Algorithm.find_by(id: params[:id])
+
+    if algorithm.nil?
+      render json: { errors: t('api.v1.algorithms.invalid_algorithm') }, status: :unprocessable_entity
+    elsif algorithm.emergency_content_version == params[:emergency_content_version].to_i
+      render json: {}, status: 204
+    else
+      render algorithm.as_json(only: [:emergency_content, :emergency_content_version])
+    end
+  end
+
 end
