7608406a01f8b9d00fa9816ee005674dc07431bb
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-09-07 15:23:52 +0000

    Update emergency route for post

--- config/routes.rb
+++ config/routes.rb
@@ -216,7 +216,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :algorithms, only: [:index] do
         member do
-          get 'emergency_content'
+          post 'emergency_content'
         end
         resources :versions, only: [:index]
       end
