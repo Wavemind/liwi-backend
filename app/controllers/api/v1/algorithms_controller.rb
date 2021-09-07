7ffa25b33e0d26448fbd7dca0ae662424319a280
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-09-07 15:45:53 +0000

    Don't verify token

--- app/controllers/api/v1/algorithms_controller.rb
+++ app/controllers/api/v1/algorithms_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::AlgorithmsController < Api::V1::ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  skip_before_action :verify_authenticity_token, only: [:emergency_content]
+
   def index
     render json: Algorithm.all.select(:id, :name, :archived, :created_at, :updated_at)
   end
 
-  # PUT /algorithms/:id/emergency_content
+  # POST /algorithms/:id/emergency_content
   # @params algorithm_id [Integer]
   # Send emergency_content if the version is not the same
   def emergency_content
