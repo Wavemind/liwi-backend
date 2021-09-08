3ab0a6efee2b744ac4a0306fc2d460ba4d88defa
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-09-08 09:19:28 +0000

    Methods to log

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,27 @@ class ApplicationRecord < ActiveRecord::Base
 
   self.abstract_class = true
 
+  after_create :log_create
+  after_update :log_update
+  before_destroy :log_delete
+
+  def log_create
+    UserLog.create(
+       user: current_user, #Gilian pour faire plaisir à Alan
+       action: 'create',
+       model: self.class,
+       id: self.id
+    )
+  end
+
+  def log_delete
+    UserLog.create(
+      user: current_user, #Gilian pour faire plaisir à Alan
+      action: 'create',
+      model: self.class,
+      id: self.id,
+      attr: self.attributes
+    )
+  end
+
 end
