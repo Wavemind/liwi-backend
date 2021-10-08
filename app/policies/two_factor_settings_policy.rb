c313195d854a76d78ca86c53472a085f9ce85114
Emmanuel <emmanuel.barchichat@wavemind.ch>
2021-10-08 10:13:33 +0000

    Add policies for 2fa

---
+++ app/policies/two_factor_settings_policy.rb
@@ -0,0 +1,23 @@
+class AlgorithmPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    user.admin? || user.clinician? || user.deployment_manager? || user.medal_r_user?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    new?
+  end
+
+  def destroy?
+    new?
+  end
+end
