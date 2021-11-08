0cf89b6a7fcc54a6846449a67ca92509a7c94e77
Manu <emmanuel.barchichat@wavemind.ch>
2021-11-08 11:31:42 +0100

    Add logs when rollback

--- app/models/version.rb
+++ app/models/version.rb
@@ -64,7 +64,7 @@ class Version < ApplicationRecord
         duplicated_version.diagnoses.each_with_index { |diagnosis, index| diagnosis.relink_instance }
         diagnoses.each { |diagnosis| diagnosis.update(duplicating: false) }
       else
-        raise ActiveRecord::Rollback, ''
+        raise ActiveRecord::Rollback, duplicated_version.errors.messages
       end
     end
   end
