dd29eec959cf55aeda36185900874677c0e54324
Manu <emmanuel.barchichat@wavemind.ch>
2022-02-11 13:39:54 +0100

    Remove manual duplication version

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -116,7 +116,6 @@ class VersionsController < ApplicationController
   # @params [Version] version to duplicate
   # Ask a job to duplicate a version with every diagnoses and their logic (Instances with their Conditions and Children), the FinalDiagnoses and Conditions attached to it
   def duplicate
-    @version.duplicate
     job_id = DuplicateVersionJob.perform_later(@version.id)
     @version.update(job_id: job_id.provider_job_id)
     render json: { success: true, job_id: job_id }
