e224123ed6545cab34188edbe8a0ca6a52701c09
Manu <emmanuel.barchichat@wavemind.ch>
2022-12-14 09:53:38 +0100

    migration for source id

---
+++ db/migrate/20221214084145_add_source_id_to_duplicated_models.rb
@@ -0,0 +1,18 @@
+class AddSourceIdToDuplicatedModels < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :diagnoses, :source, index: true
+    add_foreign_key :diagnoses, :diagnoses, column: :source_id
+
+    add_reference :nodes, :source, index: true
+    add_foreign_key :nodes, :nodes, column: :source_id
+
+    add_reference :instances, :source, index: true
+    add_foreign_key :instances, :instances, column: :source_id
+
+    add_reference :answers, :source, index: true
+    add_foreign_key :answers, :answers, column: :source_id
+
+    add_reference :conditions, :source, index: true
+    add_foreign_key :conditions, :conditions, column: :source_id
+  end
+end
